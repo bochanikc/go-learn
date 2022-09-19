906bbe911565b853f48f99285765fd1c83de7eb0
nifrolov <nifrolov@ozon.ru>
2022-09-19 17:03:50 +0300

    add TDD test for hello world

--- go-with-tests/hello.go
+++ go-with-tests/hello.go
@@ -2,10 +2,31 @@ package main
 
 import "fmt"
 
-func Hello(name string) string {
-	return fmt.Sprintf("Hello, %s!", name)
+const (
+	englishHelloPrefix = "Hello"
+	spanishHelloPrefix = "Hola"
+	frenchhHelloPrefix = "Bonjour"
+)
+
+func Hello(name, language string) string {
+	if name == "" {
+		name = "World"
+	}
+	return fmt.Sprintf("%s, %s!", getPrefix(language), name)
+}
+
+func getPrefix(language string) (prefix string) {
+	switch language {
+	case "Spanish":
+		prefix = spanishHelloPrefix
+	case "French":
+		prefix = frenchhHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+	return prefix
 }
 
 func main() {
-	fmt.Println(Hello("Chris"))
+	fmt.Println(Hello("Chris", ""))
 }
