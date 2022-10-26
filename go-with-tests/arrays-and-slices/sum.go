6a8eb7c84e3e524c00931786a1eb78bcfcf332ab
nifrolov <nifrolov@ozon.ru>
2022-10-26 17:04:32 +0300

    add tests

--- go-with-tests/arrays-and-slices/sum.go
+++ go-with-tests/arrays-and-slices/sum.go
@@ -1,1 +1,12 @@
 package arrays_and_slices
+
+func Sum(numbers []int) int {
+	sum := 0
+	//for i := 0; i < 5; i++ {
+	//	sum += numbers[i]
+	//}
+	for _, value := range numbers {
+		sum += value
+	}
+	return sum
+}
--- go-with-tests/arrays-and-slices/sum_test.go
+++ go-with-tests/arrays-and-slices/sum_test.go
@@ -1,1 +1,17 @@
 package arrays_and_slices
+
+import "testing"
+
+func TestSum(t *testing.T) {
+
+	t.Run("collection of any size", func(t *testing.T) {
+		numbers := []int{1, 2, 3}
+
+		got := Sum(numbers)
+		want := 15
+
+		if got != want {
+			t.Errorf("got %d want %d given, %v", got, want, numbers)
+		}
+	})
+}
